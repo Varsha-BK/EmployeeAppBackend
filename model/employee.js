bb57bef45a9424fb19eedccdf9ad696690707261
Varsha-BK <116425045+Varsha-BK@users.noreply.github.com>
2022-12-24 13:40:24 +0530

    Add files via upload

---
+++ model/employee.js
@@ -0,0 +1,11 @@
+var Mongoose = require("mongoose");
+
+const employeeSchema = Mongoose.Schema({
+    name: String,
+    location: String,
+    position: String,
+    salary: Number
+});
+
+var EmployeeModel = Mongoose.model("Employees",employeeSchema);
+module.exports= {EmployeeModel};
